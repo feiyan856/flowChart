6253e71b5b3d54894beae050d97590cae13fe022
kxy <kxy856@126.com>
2019-05-15 15:48:53 +0800

    去掉无用代码

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,23 +1,4 @@
 /**
- * 3D词云
- */
-declare let wordCloud: {
-  mcList: any,
-  active: any,
-  oDiv: any,
-  mouseX: any,
-  mouseY: any,
- init: () => void;
-  update: () => void;
-  depthSort: () => void;
-  positionAll: () => void;
-  doPosition: () => void;
-  sineCosine: (a: any, b: any, c: any ) => void;
-/*  mouseOverEvent: () => void;
-  mouseOutEvent: () => void;
-  mouseMoveEvent: (e: any) => void;*/
-};
-/**
  * 流程图
  */
 declare var d3;
@@ -35,28 +16,4 @@ declare let diagGraph: {
   drawEdg: () => void;
   renderG: (any) => void;
   changePoint: () => void;
-};
-declare  let dagredGraph: {
-  state: null;
-  edg: null;
-  data: null;
-  edgCount: number;
-  drawEdgCount: number;
-  statePoint: '';
-  g: '';
-  graphWidth: 0,
-  graphHeight: 0,
-  lastScale: 0,
-  inner: '',
-  t: 0,
-  svg: '',
-  dagreD3render: '',
-  init: (svgDom: any, interFrameTime: any, data: any, edg: any) => void;
-  createG: () => void;
-  renderG: () => void;
-  animateFrame: (RenderAfter: any, Frame: any) => void;
-  animateStory: (RenderAfter: any) => void;
-  drawNode: (el: any) => void;
-  drawEdg: (el: any) => void;
-  addUIAnimate: () => void;
 };
